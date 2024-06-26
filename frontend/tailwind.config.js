39b5ce8a64fe46501eb172594781b46229c1aded
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2024-06-26 22:02:33 +0200

    Feature: set basic theme color
    also create theme for dark and light mode

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
-	},
-	plugins: []
+		extend: {
+			colors: {
+				background: 'var(--color-background)',
+				text: 'var(--color-text)',
+				primary: 'var(--color-primary)',
+				secondary: 'var(--color-secondary)',
+				buttons: 'var(--color-buttons)',
+				typography: 'var(--color-typography)',
+				'background-accent': 'var(--color-background-accent)'
+			}
+		}
+	}, plugins: []
 };
