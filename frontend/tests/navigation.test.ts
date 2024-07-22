1c22de3b0d0fcacfe7f847163c945d080f21384c
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2024-07-22 20:04:01 +0200

    Test: append navigation test

---
+++ frontend/tests/navigation.test.ts
@@ -0,0 +1,19 @@
+import { expect, test } from '@playwright/test';
+
+test('application is navigable', async ({ page }) => {
+	await page.goto('/');
+	await expect(page).toHaveTitle('Api-Client :: Startseite');
+	await page.waitForSelector('footer');
+	await page.getByRole('link', { name: 'Project Overview' }).click();
+
+	await expect(page).toHaveTitle('Api-Client :: Project Overview');
+	await expect(page.locator('h2')).toBeVisible();
+	await page.getByRole('link', { name: 'first project' }).click();
+
+	await expect(page).toHaveTitle('Api-Client :: Project Details');
+	await expect(page.locator('h2')).toBeVisible();
+	await page.getByRole('link', { name: 'collection one' }).click();
+
+	await expect(page).toHaveTitle('Api-Client :: Collection Overview');
+	await expect(page.locator('h2')).toBeVisible();
+});
