b1683c99a0b07763f1eff8dd41739b2c488f0d1b
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2024-09-02 19:58:12 +0200

    Feature: auto generated files from wails

--- frontend/src/lib/wailsjs/go/models.ts
+++ frontend/src/lib/wailsjs/go/models.ts
@@ -15,3 +15,121 @@ export namespace configuration {
 
 }
 
+export namespace frontend {
+	
+	export class CollectionDto {
+	    id: number;
+	    // Go type: time
+	    updatedAt: any;
+	    name: string;
+	    projectId: number;
+	
+	    static createFrom(source: any = {}) {
+	        return new CollectionDto(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.id = source["id"];
+	        this.updatedAt = this.convertValues(source["updatedAt"], null);
+	        this.name = source["name"];
+	        this.projectId = source["projectId"];
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+	export class HttpRequestDto {
+	    id: number;
+	    // Go type: time
+	    updatedAt: any;
+	    name: string;
+	    type: string;
+	    collectionId: number;
+	    url: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new HttpRequestDto(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.id = source["id"];
+	        this.updatedAt = this.convertValues(source["updatedAt"], null);
+	        this.name = source["name"];
+	        this.type = source["type"];
+	        this.collectionId = source["collectionId"];
+	        this.url = source["url"];
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+	export class ProjectDto {
+	    id: number;
+	    // Go type: time
+	    updatedAt: any;
+	    name: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new ProjectDto(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.id = source["id"];
+	        this.updatedAt = this.convertValues(source["updatedAt"], null);
+	        this.name = source["name"];
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+
+}
+
