b9d4d2e3e1b2bf133bf4eb09abb765166569991a
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2024-09-02 20:04:03 +0200

    Feature: change props to use request store

--- frontend/src/routes/collection/[id]/+page.svelte
+++ frontend/src/routes/collection/[id]/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import Loader from '$lib/Loader.svelte';
+	import { getRequestStore } from '../../../lib/requestStore.svelte';
 
-	let { data } = $props();
+	const requestStore = getRequestStore();
 	const id: number = Number($page.params.id);
 </script>
 
@@ -11,16 +11,14 @@
 	<meta name="description" content="project overview" />
 </svelte:head>
 
-{#await data.promise}
-	<Loader />
-{:then data}
-	<h2>Collection {id}</h2>
-	<section class="flex flex-col gap-y-2">
-		{#each data.requests as request}
-			<section class="border-[1px]">
-				<p>{request.type}</p>
+<h2>Collection {id}</h2>
+<section class="flex flex-col gap-y-2">
+	{#each requestStore.getByCollectionId(id) as request}
+		<section class="border-[1px]">
+			{#if request.type === 'http'}
 				<p>{request.name}</p>
-			</section>
-		{/each}
-	</section>
-{/await}
+				<p>{request.url}</p>
+			{/if}
+		</section>
+	{/each}
+</section>
