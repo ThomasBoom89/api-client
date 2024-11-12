3985b6842dd467267dd3a0ffd5855a38a9636221
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2024-11-12 21:15:21 +0100

    Refactor: change request handling to control header

--- src/frontend/request.go
+++ src/frontend/request.go
@@ -25,37 +25,56 @@ func NewRequest(requestRepository *database.Repository[database.HttpRequest]) *R
 	return &Request{requestRepository: requestRepository}
 }
 
-func (R *Request) Submit(requestId uint) RequestResponseDTO {
+func (R *Request) Submit(requestId uint) (requestResponseDto RequestResponseDTO) {
 	httpRequest, err := R.requestRepository.GetById(requestId)
 	if err != nil {
-		return RequestResponseDTO{Error: err.Error()}
+		requestResponseDto.Error = err.Error()
+		return
 	}
 	request, err := http.NewRequest(httpRequest.Method, httpRequest.Url, nil)
 	if err != nil {
-		return RequestResponseDTO{Error: err.Error()}
+		requestResponseDto.Error = err.Error()
+		return
+	}
+	R.handleHeader(request, requestResponseDto)
+
+	client := http.Client{
+		Transport: &http.Transport{
+			DisableCompression: true,
+		},
 	}
 
-	client := new(http.Client)
 	startTime := time.Now()
 	response, err := client.Do(request)
 	endTime := time.Now()
+	requestResponseDto.ElapsedTime = endTime.Sub(startTime).String()
 	if err != nil {
-		return RequestResponseDTO{Error: err.Error()}
+		requestResponseDto.Error = err.Error()
+		return
 	}
+	requestResponseDto.ReceivedHeader = response.Header
+
 	var buffer []byte
 	buffer, err = io.ReadAll(response.Body)
 	if err != nil {
-		return RequestResponseDTO{Error: err.Error()}
+		requestResponseDto.Error = err.Error()
+		return
 	}
-	requestResponseDTO := RequestResponseDTO{
-		Error:          "",
-		Url:            request.URL.String(),
-		Method:         request.Method,
-		ResponseBody:   string(buffer),
-		SendHeader:     request.Header,
-		ReceivedHeader: response.Header,
-		ElapsedTime:    endTime.Sub(startTime).String(),
+	requestResponseDto.ResponseBody = string(buffer)
+
+	return
+}
+
+func (R *Request) handleHeader(request *http.Request, requestResponseDto RequestResponseDTO) {
+	request.Header.Set("User-Agent", "")
+	requestResponseDto.Url = request.URL.String()
+	requestResponseDto.Method = request.Method
+
+	buildHeaders := request.Header.Clone()
+	buildHeaders.Add("Host", request.Host)
+	if buildHeaders.Get("User-Agent") == "" {
+		buildHeaders.Del("User-Agent")
 	}
 
-	return requestResponseDTO
+	requestResponseDto.SendHeader = buildHeaders
 }
