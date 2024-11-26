262f5d942f738e24b90aa997de9e8e944727c858
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2024-11-26 22:09:43 +0100

    Tests: wait for application be loaded completly

--- frontend/tests/navigation.test.ts
+++ frontend/tests/navigation.test.ts
@@ -2,6 +2,8 @@ import { expect, test } from '@playwright/test';
 
 test('application is navigable', async ({ page }) => {
 	await page.goto('/');
+	const loader = page.locator('#loader');
+	await loader.waitFor({ state: 'hidden', timeout: 10000 });
 	await expect(page).toHaveTitle('Api-Client :: Startseite');
 	await page.waitForSelector('footer');
 	await page.getByRole('link', { name: 'Project Overview' }).click();
