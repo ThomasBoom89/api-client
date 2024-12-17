37cf9728ba56b68d6adceedff1f8f6fffe1b3aad
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2024-12-17 21:14:14 +0100

    test: add missing methods for stores

--- frontend/src/lib/requestStore.test.ts
+++ frontend/src/lib/requestStore.test.ts
@@ -1,9 +1,24 @@
-import { expect, test } from 'vitest';
+import { expect, test, vi } from 'vitest';
 import { frontend } from './wailsjs/go/models';
 import { RequestStore } from './requestStore.svelte';
+import * as models from '$lib/wailsjs/go/frontend/HttpRequests';
 import HttpRequestDto = frontend.HttpRequestDto;
 
-test('create request store', () => {
+test('create request store', async () => {
+	vi.spyOn(models, 'Create').mockImplementation((httpRequestDto: HttpRequestDto): Promise<HttpRequestDto> => {
+		httpRequestDto.id = 3;
+
+		return Promise.resolve(httpRequestDto);
+	});
+
+	vi.spyOn(models, 'Update').mockImplementation((httpRequestDto: HttpRequestDto): Promise<HttpRequestDto> => {
+		return Promise.resolve(httpRequestDto);
+	});
+
+	vi.spyOn(models, 'Delete').mockImplementation((httpRequestDto: HttpRequestDto): Promise<void> => {
+		return Promise.resolve();
+	});
+
 	const requestDtos = [];
 	const requestDtoOne = new HttpRequestDto(
 		'{"id":1,"updatedAt":"2024-08-28T20:49:26.369713204+02:00","name":"httpRequest0","type":"http","collectionId":1,"url":"url0"}',
@@ -12,7 +27,23 @@ test('create request store', () => {
 		'{"id":2,"updatedAt":"2024-08-28T20:49:26.369713204+02:00","name":"httpRequest1","type":"http","collectionId":2,"url":"url1"}',
 	);
 	requestDtos.push(requestDtoOne, requestDtoTwo);
-	const requestStore = new RequestStore(requestDtos);
-	expect(requestStore.getByCollectionId(1)[0]).toBe(requestDtoOne);
-	expect(requestStore.getByCollectionId(2)[0]).toBe(requestDtoTwo);
+	const httpRequestStore = new RequestStore(requestDtos);
+	expect(httpRequestStore.getByCollectionId(1)[0]).toBe(requestDtoOne);
+	expect(httpRequestStore.getByCollectionId(2)[0]).toBe(requestDtoTwo);
+
+	httpRequestStore.create(1, 'yolo');
+	await vi.waitFor(() => {
+		expect(httpRequestStore.getByCollectionId(1)[0].name).toBe('yolo');
+	});
+
+	httpRequestStore.getByCollectionId(1)[0].name = 'zwolo';
+	httpRequestStore.update(httpRequestStore.getByCollectionId(1)[0]);
+	await vi.waitFor(() => {
+		expect(httpRequestStore.getByCollectionId(1)[0].name).toBe('zwolo');
+	});
+
+	httpRequestStore.delete(httpRequestStore.getByCollectionId(1)[0]);
+	await vi.waitFor(() => {
+		expect(httpRequestStore.getByCollectionId(1).length).toBe(1);
+	});
 });
