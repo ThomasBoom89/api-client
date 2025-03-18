939e93f132ae7835f4a8895c0196650caafc5779
Nicolas <nicolas.kortgen@kornic.de>
2025-03-18 23:56:51 +0100

    added margin right to all <hr> to not colide with scrollbar

--- frontend/src/lib/ResponseDetail.svelte
+++ frontend/src/lib/ResponseDetail.svelte
@@ -22,28 +22,28 @@
 	<div class="flex flex-col h-full overflow-y-auto overflow-x-hidden p-2 border overflow">
 		<h4 class="text-text-highlight">Current Response:</h4>
 		<p class="break-words">{response.method} {response.url}</p>
-		<hr class="my-2" />
+		<hr class="my-2 mr-2" />
 		<h4 class="text-text-highlight">Status-Code:</h4>
 		<p>{response.statusCode}</p>
-		<hr class="my-2" />
+		<hr class="my-2 mr-2" />
 		<h4 class="text-text-highlight">Elapsed Time:</h4>
 		<p>{response.elapsedTime}</p>
-		<hr class="my-2" />
+		<hr class="my-2 mr-2" />
 		<h4 class="text-text-highlight">Send Header:</h4>
 		{#each Object.entries(response.sendHeader) as [key, value]}
 			<p><span class="text-text-response-headers">{key}</span> {value}</p>
 		{/each}
-		<hr class="my-2" />
+		<hr class="my-2 mr-2" />
 		<h4 class="text-text-highlight">Received Header:</h4>
 		{#each Object.entries(response.receivedHeader) as [key, value]}
 			<p class="break-words mr-2"><span class="text-text-response-headers">{key}</span> {value}</p>
 		{/each}
 		{#if response.error !== ''}
-			<hr class="my-2" />
+			<hr class="my-2 mr-2" />
 			<h4 class="text-text-highlight">Error:</h4>
 			<p>{response.error}</p>
 		{/if}
-		<hr class="my-2" />
+		<hr class="my-2 mr-2" />
 		<h4 class="text-text-highlight">Payload:</h4>
 		{#if isJsonResponse}
 			<div class="mr-2">
