659a77a4365fab407b56356dc00b0881df65f27c
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2025-04-01 20:53:48 +0200

    feat: autogenerated files from wails

--- frontend/src/lib/wailsjs/go/models.ts
+++ frontend/src/lib/wailsjs/go/models.ts
@@ -283,6 +283,65 @@ export namespace frontend {
 	        this.statusCode = source["statusCode"];
 	    }
 	}
+	export class WebsocketRequestDto {
+	    id: number;
+	    // Go type: time
+	    updatedAt: any;
+	    name: string;
+	    type: string;
+	    collectionId: number;
+	    url: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new WebsocketRequestDto(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.id = source["id"];
+	        this.updatedAt = this.convertValues(source["updatedAt"], null);
+	        this.name = source["name"];
+	        this.type = source["type"];
+	        this.collectionId = source["collectionId"];
+	        this.url = source["url"];
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+	export class WebsocketStateDto {
+	    id: number;
+	    error: string;
+	    connected: boolean;
+	    incomingMessage: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new WebsocketStateDto(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.id = source["id"];
+	        this.error = source["error"];
+	        this.connected = source["connected"];
+	        this.incomingMessage = source["incomingMessage"];
+	    }
+	}
 
 }
 
