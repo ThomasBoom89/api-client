cfaf0dbd27e51c75f62ce91600b9c938a923fff4
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2025-08-03 18:08:44 +0200

    test: compare content instead of promise

--- frontend/tests/dark-light-toggle.test.ts
+++ frontend/tests/dark-light-toggle.test.ts
@@ -4,8 +4,8 @@ test('toggle dark light mode', async ({ page }) => {
 	await page.goto('/');
 	const loader = page.locator('#loader');
 	await loader.waitFor({ state: 'hidden', timeout: 10000 });
-	const currentTheme = page.locator('html').getAttribute('data-theme');
+	const currentTheme = await page.locator('html').getAttribute('data-theme');
 	await page.getByTestId('dark-light-toggle').click();
 
-	expect(currentTheme).not.toEqual(page.locator('html').getAttribute('data-theme'));
+	expect(currentTheme).not.toEqual(await page.locator('html').getAttribute('data-theme'));
 });
